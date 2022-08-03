4af99a7b88dca85e229e6c88e5757e740b5ae181
Jinson <am987406@gmail.com>
2022-08-03 15:06:24 -0500

    Estructura e fuciond e carrito

--- js/index.js
+++ js/index.js
@@ -1,15 +1,46 @@
-document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+const carrito = document.getElementById('carrito');
+const rutas = document.getElementById('lista-carrito');
+const listaRutas = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.getElementById('vaciar-carrito');
 
-function mostrar_menu(){
+cargarEventListeners();
 
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
-    
+function cargarEventListeners () {
+    rutas.addEventListener('click', comprarRuta);
+    carrito.addEventListener('click', eliminarRuta);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+    document.addEventListener('DOMContentLoaded', leerLocalStorage)
+
 }
 
-window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
-    elemento1.style.bottom = posicion * 0.1 + "px";
-    elemento2.style.top = posicion * 0.1 + "px";
+function comprarRuta(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const ruta = e.target.parentElement.parentElement;
+        leerDatosRuta(ruta);
+    }
+}
+
+function leerDatosRuta(ruta) {
+    const inforuta ={
+        imagen: ruta.querySelector('img').src,
+        titulo: ruta.querySelector('h4').textContent,
+        precio: ruta.querySelector('.precio span').textContent,
+        id: ruta.querySelector('a').getAtribute('date-id')
+    }
+    insertarCarrito(inforuta);
+}
+
+function insertarCarrito(ruta) {
+    const row = document.createElement('tr');
+    row.innerHTML =`
+        <td>
+            <img src='$'{ruta.imagen}' width=100>
+        </td>
+        <td>${ruta.titulo}</td>
+        <td>${ruta.precio}</td>
+        <td>
+           <a href="#" class="borrar-ruta" data-id="${ruta.id}"> X</a>
+        </td>
+    `;
 }
